81eac1839042e4fad3ddb0f0a2c42dca42acd8e0
jibachh <jibachhmahato@gmail.com>
2023-10-10 17:22:11 +0545

    new feature is added

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log("Welcome to Spotify");
+ let songs = [
+    {songName:" saleme-ishq", FilePath: "song/1.mp3", coverPath:"covers/1.jpg"},
+    {songName:" saleme-ishq", FilePath: "song/1.mp3", coverPath:"covers/1.jpg"},
+    {songName:" saleme-ishq", FilePath: "song/1.mp3", coverPath:"covers/1.jpg"},
+    {songName:" saleme-ishq", FilePath: "song/1.mp3", coverPath:"covers/1.jpg"},
+    {songName:" saleme-ishq", FilePath: "song/1.mp3", coverPath:"covers/1.jpg"}
+ ]
